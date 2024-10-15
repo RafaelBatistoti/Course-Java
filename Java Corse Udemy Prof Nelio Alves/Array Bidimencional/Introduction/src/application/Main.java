47faeec42e976b30a3db3ad2b8ce349dffa0bc9a
RafaelBatistoti <rafael.batistoti.luiz@gmail.com>
2024-10-15 07:00:14 -0300

    Introduction about array bidimentional, array 2D

---
+++ Java Corse Udemy Prof Nelio Alves/Array Bidimencional/Introduction/src/application/Main.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		int[][] mat = new int[n][n];
+
+//		Loop que percorre as linhas
+		for (int i = 0; i < mat.length; i++) {
+//			Loop que percorre as colunas
+			for (int j = 0; j < mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+
+		System.out.println("Main diagonal: ");
+		for (int i = 0; i < mat.length; i++) {
+			System.out.print(mat[i][i] + " ");
+		}
+
+		int conta = 0;
+		
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				if(mat[i][j] < 0)
+					conta ++;
+			}
+		}
+		
+		System.out.println();
+		System.out.println("Quantidades de números negativos são");
+		System.out.println(conta);
+		
+		sc.close();
+
+	}
+
+}
