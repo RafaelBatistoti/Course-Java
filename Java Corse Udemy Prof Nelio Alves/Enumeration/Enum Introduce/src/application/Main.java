fbb2fb5c4f8f23ea5ae3b950a2fac6e0737fcff8
RafaelBatistoti <rafael.batistoti.luiz@gmail.com>
2024-10-20 05:09:08 -0300

    Convert String to ENUM type

--- Java Corse Udemy Prof Nelio Alves/Enumeration/Enum Introduce/src/application/Main.java
+++ Java Corse Udemy Prof Nelio Alves/Enumeration/Enum Introduce/src/application/Main.java
@@ -1,9 +1,26 @@
 package application;
 
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//													 Converted ENUM to String
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+		
+		System.out.println(order);
+		
+//		Converted String to ENUM
+		OrderStatus od1 = OrderStatus.DELIVERED;
+		OrderStatus od2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(od1);
+		System.out.println(od2);
+		
+		
 
 	}
 
