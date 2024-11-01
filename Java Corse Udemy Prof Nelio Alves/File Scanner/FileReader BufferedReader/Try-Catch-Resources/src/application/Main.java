a8334b2b9b988658d8d69031300318eb9d1fff28
RafaelBatistoti <rafael.batistoti.luiz@gmail.com>
2024-11-01 09:28:58 -0300

    Introduce about FileReader and BufferedReader and working with try/catch resources

---
+++ Java Corse Udemy Prof Nelio Alves/File Scanner/FileReader BufferedReader/Try-Catch-Resources/src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String pathName = "in.txt";
+
+
+		try (BufferedReader bf = new BufferedReader(new FileReader(pathName))){
+
+			String line = bf.readLine(); 	// Lê o arquivo até o final.
+										 	// Caso ele leia uma linha vazia vai ocorrer valores nulos
+
+			while (line != null) { 			// Condição para ir lendo até ser diferente de nullo
+				System.out.println(line); 	// Iprime a linha
+				line = bf.readLine(); 		// Pega a próxima linha
+			}
+
+		}
+		catch (IOException e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
+		
+	}
+
+}
