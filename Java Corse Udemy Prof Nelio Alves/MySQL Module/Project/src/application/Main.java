3cfca2408b8367234fd5c0eaa26509bd80973736
RafaelBatistoti <rafael.batistoti.luiz@gmail.com>
2024-12-27 14:36:03 -0300

    Start a project with MySQL - Created the Seller class

--- Java Corse Udemy Prof Nelio Alves/MySQL Module/Project/src/application/Main.java
+++ Java Corse Udemy Prof Nelio Alves/MySQL Module/Project/src/application/Main.java
@@ -1,14 +1,24 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		DateTimeFormatter dateTimeFomatted = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
 		Department obj = new Department(1, "Books");
 		
+		LocalDate birthDate = LocalDate.parse("02/03/1992", dateTimeFomatted);
 		
+		Seller sl = new Seller(1, "Rafael", "rafael@gmail.com", birthDate , 2000.0, obj);
+		
+		System.out.println(sl);
 
 	}
 
