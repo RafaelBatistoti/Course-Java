5bd42a8b413b01501858e270b6c706d89ec6df70
RafaelBatistoti <rafael.batistoti.luiz@gmail.com>
2024-12-27 14:59:19 -0300

    Start a project with MySQL - Created the DaoFactory

--- Java Corse Udemy Prof Nelio Alves/MySQL Module/Project/src/application/Main.java
+++ Java Corse Udemy Prof Nelio Alves/MySQL Module/Project/src/application/Main.java
@@ -3,21 +3,25 @@ package application;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		DateTimeFormatter dateTimeFomatted = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
 		Department obj = new Department(1, "Books");
-		
+
 		LocalDate birthDate = LocalDate.parse("02/03/1992", dateTimeFomatted);
-		
-		Seller sl = new Seller(1, "Rafael", "rafael@gmail.com", birthDate , 2000.0, obj);
-		
+
+		Seller sl = new Seller(1, "Rafael", "rafael@gmail.com", birthDate, 2000.0, obj);
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+
 		System.out.println(sl);
 
 	}
