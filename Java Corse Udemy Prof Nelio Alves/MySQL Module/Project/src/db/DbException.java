27fcf465decc976efc94f92e152681ab2c372bdc
RafaelBatistoti <rafael.batistoti.luiz@gmail.com>
2024-12-27 13:35:55 -0300

    Start a project with MySQL - Created the Department class

--- Java Corse Udemy Prof Nelio Alves/MySQL Module/Project/src/db/DbException.java
+++ Java Corse Udemy Prof Nelio Alves/MySQL Module/Project/src/db/DbException.java
@@ -1,4 +1,4 @@
-package entities.db;
+package db;
 
 public class DbException extends RuntimeException{
 	
--- Java Corse Udemy Prof Nelio Alves/MySQL Module/Project/src/db/DbIntegrityException.java
+++ Java Corse Udemy Prof Nelio Alves/MySQL Module/Project/src/db/DbIntegrityException.java
@@ -1,4 +1,4 @@
-package entities.db;
+package db;
 
 public class DbIntegrityException extends RuntimeException{
 	
