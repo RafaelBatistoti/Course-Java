fcaa2531c3bf9d9b8bc352ef12ec710fe9d82065
RafaelBatistoti <rafael.batistoti.luiz@gmail.com>
2025-01-02 08:51:53 -0300

    Start a project with JPA/Hibernate - remove values in database

---
+++ Java Corse Udemy Prof Nelio Alves/Java EE/Maven/Introduce Maven - remove data/src/main/java/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager        em  = emf.createEntityManager();
+
+		Pessoa pessoa = em.find(Pessoa.class, 5);
+		
+		em.getTransaction().begin();
+		em.remove(pessoa);
+		em.getTransaction().commit();
+
+		System.out.println(pessoa);
+
+		emf.close();
+		em.close();
+	}
+
+}
