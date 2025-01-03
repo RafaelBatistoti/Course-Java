5c2e4a37a868c795b9d8bf07e3c338c05a316f55
RafaelBatistoti <rafael.batistoti.luiz@gmail.com>
2025-01-03 04:51:23 -0300

    Spring boot Project - inicialize

---
+++ Java Corse Udemy Prof Nelio Alves/Java EE/Projeto_completo_SpringBoot/Projeto_completo_SpringBoot/src/main/java/com/udemy/Projeto_completo_SpringBoot/ProjetoCompletoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.Projeto_completo_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCompletoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCompletoSpringBootApplication.class, args);
+	}
+
+}
---
+++ Java Corse Udemy Prof Nelio Alves/Java EE/Projeto_completo_SpringBoot/Projeto_completo_SpringBoot/src/test/java/com/udemy/Projeto_completo_SpringBoot/ProjetoCompletoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.Projeto_completo_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCompletoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
