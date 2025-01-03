5fa4bce9162a15b36274a60de8c248fdff19e11a
RafaelBatistoti <rafael.batistoti.luiz@gmail.com>
2025-01-03 05:10:10 -0300

    Spring boot Project - Create method user entity and user resource

---
+++ Java Corse Udemy Prof Nelio Alves/Java EE/Projeto_completo_SpringBoot/Projeto_completo_SpringBoot/src/main/java/com/udemy/Projeto_completo_SpringBoot/resources/UserResources.java
@@ -0,0 +1,20 @@
+package com.udemy.Projeto_completo_SpringBoot.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.udemy.Projeto_completo_SpringBoot.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResources {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User user = new User(1L,"Rafael", "rafael@gmail.com", "(41)99699-0348", "Nokia");
+		return ResponseEntity.ok().body(user);
+	}
+
+}
