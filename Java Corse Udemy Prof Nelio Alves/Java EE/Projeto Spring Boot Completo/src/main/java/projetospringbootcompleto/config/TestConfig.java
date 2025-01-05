3a4ad37a17ec8f1f24d597042646f7254e961334
RafaelBatistoti <rafael.batistoti.luiz@gmail.com>
2025-01-05 08:20:45 -0300

    Spring boot Project - Implement the ENUNM class to control the orders status

--- Java Corse Udemy Prof Nelio Alves/Java EE/Projeto Spring Boot Completo/src/main/java/projetospringbootcompleto/config/TestConfig.java
+++ Java Corse Udemy Prof Nelio Alves/Java EE/Projeto Spring Boot Completo/src/main/java/projetospringbootcompleto/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import projetospringbootcompleto.entities.Order;
 import projetospringbootcompleto.entities.User;
+import projetospringbootcompleto.enums.OrderStatus;
 import projetospringbootcompleto.repositories.OrderRepository;
 import projetospringbootcompleto.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 		User user_1 = new User(null, "Mariano", "mariano@gmail.com", "(41)99699-0348", "Brasil");
 		User user_2 = new User(null, "Macareno", "macareno@gmail.com", "(41)99856-1204", "Motorola");
 
-		Order order_1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user_1);
-		Order order_2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user_2);
-		Order order_3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user_1);
+		Order order_1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user_1);
+		Order order_2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user_2);
+		Order order_3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user_1);
 
 		userRepository.saveAll(Arrays.asList(user_1, user_2));
 		orderRepository.saveAll(Arrays.asList(order_1, order_2, order_3));
--- Java Corse Udemy Prof Nelio Alves/Java EE/Projeto Spring Boot Completo/src/main/java/projetospringbootcompleto/entities/Order.java
+++ Java Corse Udemy Prof Nelio Alves/Java EE/Projeto Spring Boot Completo/src/main/java/projetospringbootcompleto/entities/Order.java
@@ -5,7 +5,6 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,9 +13,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import projetospringbootcompleto.enums.OrderStatus;
 
 @Entity
-@Table(name="tb_order")
+@Table(name = "tb_order")
 public class Order implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -24,11 +24,12 @@ public class Order implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 
-	@JsonIgnore
+	private Integer status;
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -37,10 +38,11 @@ public class Order implements Serializable {
 
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus status, User client) {
 		super();
 		this.id     = id;
 		this.moment = moment;
+		setStatus(status);
 		this.client = client;
 	}
 
@@ -66,6 +68,16 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public OrderStatus getStatus() {
+		return OrderStatus.valueOf(status);
+	}
+
+	public void setStatus(OrderStatus status) {
+		if (status != null) {
+			this.status = status.getCode();
+		}
 	}
 
 	@Override
