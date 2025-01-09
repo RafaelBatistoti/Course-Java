27fa1521519b0560ce25659d42545c9cf83f3dfb
RafaelBatistoti <rafael.batistoti.luiz@gmail.com>
2025-01-09 14:27:06 -0300

    Spring boot Project - implements subtotal and total methods in Json

--- Java Corse Udemy Prof Nelio Alves/Java EE/Projeto Spring Boot Completo/src/main/java/projetospringbootcompleto/entities/Order.java
+++ Java Corse Udemy Prof Nelio Alves/Java EE/Projeto Spring Boot Completo/src/main/java/projetospringbootcompleto/entities/Order.java
@@ -42,7 +42,7 @@ public class Order implements Serializable {
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 
-	@OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
 
 	public Order() {
@@ -100,6 +100,14 @@ public class Order implements Serializable {
 
 	public void setPayment(Payment payment) {
 		this.payment = payment;
+	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem orderItem : items) {
+			sum += orderItem.getSubTotal();
+		}
+		return sum;
 	}
 
 	@Override
